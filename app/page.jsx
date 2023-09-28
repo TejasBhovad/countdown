8ec118f0e8c14c505b42f6e474dbe095fa84a115
rj22ad1036 <rohanjoshijoshi50@gmail.com>
2023-09-28 14:00:20 +0530

    sortby

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,16 @@
 // import statements go here REMOVE UNNECESSARY IMPORTS
 import { React, useState } from "react";
 import EventFrame from "@/components/EventFrame.jsx";
-import { Button } from "@/components/ui/button";
+import Sortby from "@/components/Sortby";
 
 const page = () => {
   return (
     <div className="bg-background w-full h-full">
       {/* Added element for testing purposes, after testing should be removed from here */}
       <EventFrame />
+      <Sortby />
     </div>
+   
   );
 };
 
---
+++ components/Sortby.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const sortby = () => {
+  return (
+    <div>sortby</div>
+  )
+}
+
+export default sortby

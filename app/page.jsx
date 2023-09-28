0bf1d2b1d57d009a788876782faaf77d580731db
Jay Darje <114568110+TheDeadMax2@users.noreply.github.com>
2023-09-28 14:16:22 +0530

    Sortby Removed

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,12 @@
 // import statements go here REMOVE UNNECESSARY IMPORTS
 import { React, useState } from "react";
 import EventFrame from "@/components/EventFrame.jsx";
-import Sortby from "@/components/Sortby";
 
 const page = () => {
   return (
     <div className="bg-background w-full h-full">
       {/* Added element for testing purposes, after testing should be removed from here */}
       <EventFrame />
-      <Sortby />
     </div>
    
   );

6c97799615248a179760fb6c2aa4095181132a56
rj22ad1036 <rohanjoshijoshi50@gmail.com>
2023-09-30 10:58:33 +0530

    designbar(1)

--- components/Designbar.jsx
+++ components/Designbar.jsx
@@ -3,37 +3,37 @@ import React from 'react'
 const Designbar = () => {
   return (
    
-    <div className='flex-col bg-blue-700 rounded-lg w-56'>
-    <div className='title text-bold text-xl r bg-primary w-56 justify-center grid-cols-1 text-white p-5 '>Countdown Name</div>
+    <div className='flex-col bg-blue-700 rounded-lg w-80'>
+    <div className='title text-bold text-xl r bg-primary w-80 justify-center grid-cols-1 text-white p-5 '>Countdown Name</div>
     <div className='flex p-5'>
         <div className='font-black pr-4 '>EventDate  </div>
-       <div  > <input type='datetime-local'  className='w-20 rounded-md bg-white' /></div>
+       <div  > <input type='datetime-local'  className=' rounded-md bg-white' /></div>
         </div>
         
         
     <div className='flex  p-5'>
         <div className='font-black pr-5'>StartDate  </div>
-       <div > <input type='time'  className='w-20 rounded-md bg-white ' /></div>
+       <div > <input type='time'  className=' rounded-md bg-white ' /></div>
         </div>
          
         <div className='flex  p-5'>
         <div className='font-black pr-7'>EndDate  </div>
-       <div > <input type='time'  className='w-20 rounded-md bg-white ' /></div>
+       <div > <input type='time'  className=' rounded-md bg-white ' /></div>
         </div>
          
         <div className='flex  p-5'>
-        <div className='font-black pr-5'> PrimarycolorColor </div>
+        <div className='font-black pr-20'> PrimaryColor </div>
        <div > <input type='color' className='w-5 h-5 rounded-md bg-white' /></div>
         </div>
         
 
         <div className='flex  p-5'>
-        <div className='font-black pr-8'> SecondaryColor </div>
+        <div className='font-black pr-14'> SecondaryColor </div>
        <div > <input type='color' className='w-5 h-5 rounded-md bg-white' /></div>
         </div>
         
         <div className='flex  p-5'>
-        <div className='font-black pr-20'> TextColor  </div>
+        <div className='font-black pr-28'> TextColor  </div>
        <div > <input type='color' className='w-5 h-5 rounded-md bg-white' /></div>
         </div>
     </div>

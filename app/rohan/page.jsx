61acc34b0186a6323a531680e7a80012efdf3406
rj22ad1036 <rohanjoshijoshi50@gmail.com>
2023-10-03 23:18:58 +0530

    Textbar

--- app/rohan/page.jsx
+++ app/rohan/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 // import statements go here REMOVE UNNECESSARY IMPORTS
 import { React, useState } from "react";
-import BasicInfo from "@/components/BasicInfo";
+import Textbar from "@/components/Textbar";
 
 
 
@@ -12,7 +12,7 @@ const page = () => {
       {/* Added element for testing purposes, after testing should be removed from here */}
      
      
-  <BasicInfo/>
+  <Textbar/>
      
     </div>
   );
